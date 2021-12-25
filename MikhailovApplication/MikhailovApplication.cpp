39b4bf32ca83268befa6303d8d5a1a0b3d6c1e51
пуке <aaa@mail.ru>
2021-12-25 12:15:43 +0700

    ukuyi

---
+++ MikhailovApplication/MikhailovApplication.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+    char mask[] = "39:19:9999";
+    char text[256];
+    std::cin >> text;
+    int i = 0;
+    int j = 0;
+    while (text[i] != '\0') {
+        for (j = 0; j < 10; j++) {
+            if (text[i + j] == '\0') {
+                std::cout << "- 1";
+                return -1;
+            }
+            if (mask[j] - '0' <= 9) {
+                if ((text[i + j] - '0') > (mask[j] - '0'))
+                    break;
+            }
+            else if (text[i + j] != mask[j]) break;
+        }
+        if (j == 10) {
+            std::cout << i;
+            return i;
+        }
+        i++;
+    }
+}
